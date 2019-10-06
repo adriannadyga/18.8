34398d01a7e8d38bdaad951e221b4c6d1650f085
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-10-06 21:48:57 +0200

    Add refractory

--- components/App.js
+++ components/App.js
@@ -1,5 +1,7 @@
+var GIPHY_API_URL = 'https://api.giphy.com';
+var GIPHY_PUB_KEY = 'fKzLWXW1Fhec0lzGrlx5GOfgEUFOueFw';
+
 App = React.createClass({    
-    
     getInitialState() {
         return {
             loading: false,
@@ -8,25 +10,45 @@ App = React.createClass({
         };
     },      
         
-    getGif: function(searchingText, callback) {  
-        var GIPHY_API_URL = 'https://api.giphy.com';
-        var GIPHY_PUB_KEY = 'fKzLWXW1Fhec0lzGrlx5GOfgEUFOueFw';
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  
-        var xhr = new XMLHttpRequest();  
-        xhr.open('GET', url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-               var data = JSON.parse(xhr.responseText).data; 
-                var gif = {  
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
-                };
-                callback(gif);  
-            }
-        };
-        xhr.send();
-    },    
-        
+    // getGif: function(searchingText, callback) {  
+    //     var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  
+    //     var xhr = new XMLHttpRequest();  
+    //     xhr.open('GET', url);
+    //     xhr.onload = function() {
+    //         if (xhr.status === 200) {
+    //            var data = JSON.parse(xhr.responseText).data; 
+    //             var gif = {  
+    //                 url: data.fixed_width_downsampled_url,
+    //                 sourceUrl: data.url
+    //             };
+    //             callback(gif);  
+    //         }
+    //     };
+    //     xhr.send();
+    // }, 
+    
+    getGif: function(searchingText) {
+      const url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+      return new Promise(
+          function(resolve, reject) {
+              let request = new XMLHttpRequest();
+              request.onload = function() {
+                  if (this.status === 200) {
+                    let data = JSON.parse(this.responseText).data; 
+                      resolve(data); // Sukces
+                  } else {
+                      reject(new Error(this.statusText)); // Dostaliśmy odpowiedź, ale jest to np 404
+                  }
+              };
+              request.onerror = function() {
+                  reject(new Error(
+                     `XMLHttpRequest Error: ${this.statusText}`));
+              };
+              request.open('GET', url);
+              request.send();
+          });
+  },
+
     handleSearch: function(searchingText) {  
         this.setState({
           loading: true  
@@ -41,7 +63,7 @@ App = React.createClass({
       },    
         
         render: function() {
-    
+            //style inline
             var styles = {
                 margin: '0 auto',
                 textAlign: 'center',
